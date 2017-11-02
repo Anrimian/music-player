b61dd6521513ffb56396c161ce4bcede411e5adf
Vladislav <vladislav.slesaryonok@oz.by>
2017-11-02 14:35:43 +0300

    + create ui test(not working)

--- presentation/src/androidTest/java/com/github/anrimian/simplemusicplayer/ui/library/storage/StorageLibraryFragmentTest.java
+++ presentation/src/androidTest/java/com/github/anrimian/simplemusicplayer/ui/library/storage/StorageLibraryFragmentTest.java
@@ -1,13 +1,21 @@
 package com.github.anrimian.simplemusicplayer.ui.library.storage;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.github.anrimian.simplemusicplayer.R;
+import com.github.anrimian.simplemusicplayer.ui.main.MainActivity;
+
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Created on 29.10.2017.
@@ -19,18 +27,29 @@ public class StorageLibraryFragmentTest {
     private StorageLibraryPresenter presenter;
     private StorageLibraryFragment fragment;
 
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Before
     public void setUp() {
         presenter = mock(StorageLibraryPresenter.class);
+        fragment = new StorageLibraryFragment() {
+            @Override
+            StorageLibraryPresenter providePresenter() {
+                return presenter;
+            }
+        };
 
-        fragment = StorageLibraryFragment.newInstance(null);
-        when(fragment.providePresenter()).thenReturn(presenter);
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction();
     }
 
     @Test
     public void testTest() {
-        presenter.onFirstViewAttach();
+        //presenter.onFirstViewAttach();
 
+        onView(withId(R.id.psv_progress_bar)).check(matches((isDisplayed())));
 //        Assert.assertNotNull();
     }
 
