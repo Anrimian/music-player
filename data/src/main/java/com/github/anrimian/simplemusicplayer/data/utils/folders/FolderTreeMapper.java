027baa9d17a70bbe7fa4d0380c96346727e09775
Mr. Noname <info@mrsoft.by>
2018-03-13 18:01:19 +0300

    + more folder utils for future

---
+++ data/src/main/java/com/github/anrimian/simplemusicplayer/data/utils/folders/FolderTreeMapper.java
@@ -0,0 +1,30 @@
+package com.github.anrimian.simplemusicplayer.data.utils.folders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FolderTreeMapper {
+
+//    public FolderTreeInfo toTreeInfo(RxNode<Long, Folder> node) {
+//        List<FolderNode> folderNodes = new ArrayList<>();
+//        for (RxNode<Long, Folder> child: node.getNodes()) {
+//            folderNodes.add(toFolderNode(child));
+//        }
+//        FolderTreeInfo folderTreeInfo = new FolderTreeInfo();
+//        folderTreeInfo.setNodes(folderNodes);
+//        folderTreeInfo.setChangeObservable(node.getChangeObservable().map(this::toFolderNode));
+//        return folderTreeInfo;
+//    }
+//
+//    private Change<FolderNode> toFolderNode(Change<RxNode<Long, Folder>> rxChange) {
+//        return new Change<>(rxChange.getChangeType(), toFolderNode(rxChange.getData()));
+//    }
+//
+//    private FolderNode toFolderNode(RxNode<Long, Folder> rxNode) {
+//        FolderNode folderNode = new FolderNode();
+//        folderNode.setFolder(rxNode.getData());
+//        folderNode.setHasChildren(!rxNode.getNodes().isEmpty());
+//        return folderNode;
+//    }
+}
