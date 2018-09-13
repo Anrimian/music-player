6ac6fcf4842dc569ce05fc4be4758e7883080e55
Vladislav <anrimian@gmail.com>
2018-09-13 23:48:34 +0300

    * fix MediaDataRetriever memory leak(?)

--- presentation/src/main/java/com/github/anrimian/musicplayer/ui/common/format/ImageFormatUtils.java
+++ presentation/src/main/java/com/github/anrimian/musicplayer/ui/common/format/ImageFormatUtils.java
@@ -3,10 +3,13 @@ package com.github.anrimian.musicplayer.ui.common.format;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.github.anrimian.musicplayer.R;
 import com.github.anrimian.musicplayer.domain.models.composition.Composition;
+
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -17,21 +20,31 @@ import static com.github.anrimian.musicplayer.ui.utils.ImageUtils.toCircleBitmap
 public class ImageFormatUtils {
 
     public static void displayImage(ImageView imageView, Composition composition) {
+        imageView.setImageResource(R.drawable.ic_music_placeholder);
         Single.fromCallable(() -> getCompositionImage(composition))
-                .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(imageView::setImageBitmap,
                         t -> imageView.setImageResource(R.drawable.ic_music_placeholder));
     }
 
     private static Bitmap getCompositionImage(Composition composition) {
-        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-        mmr.setDataSource(composition.getFilePath());
-        byte[] imageBytes = mmr.getEmbeddedPicture();
-        if (imageBytes == null) {
-            throw new RuntimeException("not found");
+        MediaMetadataRetriever mmr = null;
+        try {
+            mmr = new MediaMetadataRetriever();
+            mmr.setDataSource(composition.getFilePath());
+            byte[] imageBytes = mmr.getEmbeddedPicture();
+            mmr.release();
+            if (imageBytes == null) {
+                throw new RuntimeException("not found");
+            }
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            return toCircleBitmap(bitmap);
+        } finally {
+            if (mmr != null) {
+                mmr.release();
+            }
         }
-        Bitmap bitmap =  BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-        return toCircleBitmap(bitmap);
+
     }
 }
