961fed1b0b6b536483bb2e595914277551a52fac
Vladislav <anrimian@gmail.com>
2019-03-27 19:52:48 +0300

    * fix music notification on apiV < 24

--- presentation/src/main/java/com/github/anrimian/musicplayer/di/app/AppModule.java
+++ presentation/src/main/java/com/github/anrimian/musicplayer/di/app/AppModule.java
@@ -1,12 +1,15 @@
 package com.github.anrimian.musicplayer.di.app;
 
 import android.content.Context;
+import android.os.Build;
 
 import com.github.anrimian.musicplayer.domain.business.analytics.Analytics;
 import com.github.anrimian.musicplayer.domain.controllers.SystemServiceController;
 import com.github.anrimian.musicplayer.infrastructure.analytics.AnalyticsImpl;
 import com.github.anrimian.musicplayer.infrastructure.service.SystemServiceControllerImpl;
 import com.github.anrimian.musicplayer.ui.notifications.NotificationsDisplayer;
+import com.github.anrimian.musicplayer.ui.notifications.builder.AppNotificationBuilder;
+import com.github.anrimian.musicplayer.ui.notifications.builder.AppNotificationBuilderV21;
 
 import javax.annotation.Nonnull;
 import javax.inject.Singleton;
@@ -37,8 +40,19 @@ public class AppModule {
     @Provides
     @Nonnull
     @Singleton
-    NotificationsDisplayer provideNotificationsController(Context context) {
-        return new NotificationsDisplayer(context);
+    NotificationsDisplayer provideNotificationsController(Context context,
+                                                          AppNotificationBuilder notificationBuilder) {
+        return new NotificationsDisplayer(context, notificationBuilder);
+    }
+
+    @Provides
+    @Nonnull
+    @Singleton
+    AppNotificationBuilder appNotificationBuilder() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return new AppNotificationBuilder();
+        }
+        return new AppNotificationBuilderV21();
     }
 
     @Provides
