74332b3d61ff469f8eeee5eedfa6522da34a75a8
Vladislav <anrimian@gmail.com>
2019-06-02 20:28:03 +0300

    + small widget layout

---
+++ presentation/src/main/java/com/github/anrimian/musicplayer/ui/widgets/WidgetProvider.java
@@ -0,0 +1,8 @@
+package com.github.anrimian.musicplayer.ui.widgets;
+
+import android.appwidget.AppWidgetProvider;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+
+}
---
+++ presentation/src/main/java/com/github/anrimian/musicplayer/ui/widgets/WidgetProviderSmall.java
@@ -0,0 +1,8 @@
+package com.github.anrimian.musicplayer.ui.widgets;
+
+import android.appwidget.AppWidgetProvider;
+
+public class WidgetProviderSmall extends AppWidgetProvider {
+
+
+}
