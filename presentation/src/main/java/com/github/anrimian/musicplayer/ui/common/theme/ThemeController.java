274d92d813d99b0e4a7e60c869a65269db070721
Vladislav <anrimian@gmail.com>
2019-07-01 14:32:18 +0300

    + dark theme(5)

--- presentation/src/main/java/com/github/anrimian/musicplayer/ui/common/theme/ThemeController.java
+++ presentation/src/main/java/com/github/anrimian/musicplayer/ui/common/theme/ThemeController.java
@@ -1,14 +1,12 @@
 package com.github.anrimian.musicplayer.ui.common.theme;
 
 import android.app.Activity;
-import android.app.ActivityManager;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 
 import com.github.anrimian.musicplayer.R;
 import com.github.anrimian.musicplayer.data.utils.preferences.SharedPreferencesHelper;
+import com.github.anrimian.musicplayer.ui.utils.AndroidUtils;
 
 import static com.github.anrimian.musicplayer.ui.utils.AndroidUtils.getColorFromAttr;
 
@@ -42,20 +40,9 @@ public class ThemeController {
     }
 
     private void updateTaskManager(Activity activity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ActivityManager.TaskDescription taskDescription;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                taskDescription = new ActivityManager.TaskDescription(
-                        activity.getString(R.string.app_name),
-                        R.mipmap.ic_launcher,
-                        getColorFromAttr(activity, R.attr.colorPrimary));
-            } else {
-                taskDescription = new ActivityManager.TaskDescription(
-                        activity.getString(R.string.app_name),
-                        BitmapFactory.decodeResource(activity.getResources(), R.mipmap.ic_launcher),
-                        getColorFromAttr(activity, R.attr.colorPrimary));
-            }
-            activity.setTaskDescription(taskDescription);
-        }
+        AndroidUtils.updateTaskManager(activity,
+                R.string.app_name,
+                R.mipmap.ic_launcher,
+                getColorFromAttr(activity, R.attr.colorPrimary));
     }
 }
