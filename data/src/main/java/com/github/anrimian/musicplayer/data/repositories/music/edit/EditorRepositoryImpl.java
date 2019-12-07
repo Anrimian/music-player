bf19d03c2af148e24458b5eca74a46d9a9e41ffa
Vladislav <anrimian@gmail.com>
2019-12-08 01:14:55 +0300

    * fix ui elements size to suggested by google play * fix error message on try move from root to root folder

--- data/src/main/java/com/github/anrimian/musicplayer/data/repositories/music/edit/EditorRepositoryImpl.java
+++ data/src/main/java/com/github/anrimian/musicplayer/data/repositories/music/edit/EditorRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.github.anrimian.musicplayer.data.storage.providers.music.StorageMusic
 import com.github.anrimian.musicplayer.domain.models.composition.Composition;
 import com.github.anrimian.musicplayer.domain.repositories.EditorRepository;
 import com.github.anrimian.musicplayer.domain.utils.FileUtils;
+import com.github.anrimian.musicplayer.domain.utils.Objects;
 
 import java.io.File;
 import java.util.List;
@@ -62,7 +63,7 @@ public class EditorRepositoryImpl implements EditorRepository {
 
     @Override
     public Single<String> moveFile(String filePath, String oldPath, String newPath) {
-        if (oldPath.equals(newPath)) {
+        if (Objects.equals(oldPath, newPath)) {
             return Single.error(new MoveInTheSameFolderException("move in the same folder"));
         }
         return Single.fromCallable(() -> FileUtils.getChangedFilePath(filePath, oldPath, newPath))
