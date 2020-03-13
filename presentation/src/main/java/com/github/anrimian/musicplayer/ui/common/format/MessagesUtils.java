df260aa2f60583aee6cf3a2ab3fa2a59b6fb6b9e
Anrimian <anrimian@gmail.com>
2020-03-13 17:13:56 +0300

    * fix message texts

--- presentation/src/main/java/com/github/anrimian/musicplayer/ui/common/format/MessagesUtils.java
+++ presentation/src/main/java/com/github/anrimian/musicplayer/ui/common/format/MessagesUtils.java
@@ -27,40 +27,49 @@ public class MessagesUtils {
     public static String getAddToPlayListCompleteMessage(Context context,
                                                          PlayList playList,
                                                          List<Composition> compositions) {
-        if (compositions.size() == 1) {
+        int size = compositions.size();
+        if (size == 1) {
             return context.getString(R.string.add_to_playlist_success_template,
                     formatCompositionName(compositions.get(0)),
                     playList.getName());
         } else {
-            //TODO quantity string
-            return context.getString(R.string.add_to_playlist_count_success_template,
-                    compositions.size(),
-                    playList.getName());
+            return context.getResources().getQuantityString(
+                    R.plurals.add_to_playlist_count_success_template,
+                    size,
+                    size,
+                    playList.getName()
+            );
         }
     }
 
     public static String getDeletePlayListItemCompleteMessage(Context context,
                                                               PlayList playList,
                                                               List<PlayListItem> items) {
-        if (items.size() == 1) {
+        int size = items.size();
+        if (size == 1) {
             return context.getString(R.string.delete_from_playlist_success_template,
                     formatCompositionName(items.get(0).getComposition()),
                     playList.getName());
         } else {
-            //TODO quantity string
-            return context.getString(R.string.delete_from_playlist_count_success_template,
-                    items.size(),
+            return context.getResources().getQuantityString(
+                    R.plurals.delete_from_playlist_count_success_template,
+                    size,
+                    size,
                     playList.getName());
         }
     }
 
     public static String getDeleteCompleteMessage(Context context, List<Composition> compositions) {
-        if (compositions.size() == 1) {
+        int size = compositions.size();
+        if (size == 1) {
             return context.getString(R.string.delete_composition_success,
                     formatCompositionName(compositions.get(0)));
         } else {
-            //TODO quantity string
-            return context.getString(R.string.delete_compositions_success, compositions.size());
+            return context.getResources().getQuantityString(
+                    R.plurals.delete_compositions_success,
+                    size,
+                    size
+            );
         }
     }
 
