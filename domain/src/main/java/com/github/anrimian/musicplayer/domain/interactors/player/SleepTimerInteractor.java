0e536d25bb7ef7c18a3f9dc301e298af5f95627a
Vladislav <anrimian@gmail.com>
2020-11-27 12:19:23 +0300

    + sleep timer(4)

--- domain/src/main/java/com/github/anrimian/musicplayer/domain/interactors/player/SleepTimerInteractor.java
+++ domain/src/main/java/com/github/anrimian/musicplayer/domain/interactors/player/SleepTimerInteractor.java
@@ -10,41 +10,59 @@ import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.subjects.BehaviorSubject;
 
 //fade out when timer is finishing(?)
-//states: enable/disable/disable_wait_for_finish
+//handle 'play last song' option
+//states: enable/disable/paused/disable_wait_for_finish
 public class SleepTimerInteractor {
 
-//    private final LibraryPlayerInteractor libraryPlayerInteractor;
+    public static final long NO_TIMER = 1;
 
+    private final LibraryPlayerInteractor libraryPlayerInteractor;
     private final SettingsRepository settingsRepository;
     private final UiStateRepository uiStateRepository;
 
-    private final BehaviorSubject<Long> timerCountDownSubject = BehaviorSubject.create();
+    private final BehaviorSubject<Long> timerCountDownSubject = BehaviorSubject.createDefault(NO_TIMER);
+    private final BehaviorSubject<SleepTimerState> sleepTimerState = BehaviorSubject.createDefault(SleepTimerState.DISABLED);
 
     private Disposable timerDisposable;
 
-    public SleepTimerInteractor(SettingsRepository settingsRepository,
+    public SleepTimerInteractor(LibraryPlayerInteractor libraryPlayerInteractor,
+                                SettingsRepository settingsRepository,
                                 UiStateRepository uiStateRepository) {
+        this.libraryPlayerInteractor = libraryPlayerInteractor;
         this.settingsRepository = settingsRepository;
         this.uiStateRepository = uiStateRepository;
     }
 
     public void start() {
         startSleepTimer(settingsRepository.getSleepTimerTime());
+        sleepTimerState.onNext(SleepTimerState.ENABLED);
     }
 
     public void stop() {
         pause();
         uiStateRepository.setSleepTimerRemainingMillis(0L);
+        timerCountDownSubject.onNext(NO_TIMER);
+        sleepTimerState.onNext(SleepTimerState.DISABLED);
     }
 
     public void pause() {
         if (timerDisposable != null) {
             timerDisposable.dispose();
         }
+        sleepTimerState.onNext(SleepTimerState.PAUSED);
     }
 
     public void resume() {
         startSleepTimer(uiStateRepository.getSleepTimerRemainingMillis());
+        sleepTimerState.onNext(SleepTimerState.ENABLED);
+    }
+
+    public Observable<Long> getSleepTimerCountDownObservable() {
+        return timerCountDownSubject;
+    }
+
+    public Observable<SleepTimerState> getSleepTimerStateObservable() {
+        return sleepTimerState;
     }
 
     public void setPlayLastSong(boolean playLastSong) {
@@ -67,7 +85,14 @@ public class SleepTimerInteractor {
     }
 
     private void onTimerFinished() {
-        //stop music
-        //disable timer
+        libraryPlayerInteractor.pause();
+        timerCountDownSubject.onNext(NO_TIMER);
+        sleepTimerState.onNext(SleepTimerState.DISABLED);
+    }
+
+    public enum SleepTimerState {
+        ENABLED,
+        DISABLED,
+        PAUSED
     }
 }
