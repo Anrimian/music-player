d1040fee2713eb976165bb276df238503f091ff4
Vladislav <anrimian@gmail.com>
2021-06-29 13:50:50 +0300

    * possible fix for non-writable files

--- data/src/main/java/com/github/anrimian/musicplayer/data/storage/source/CompositionSourceEditor.java
+++ data/src/main/java/com/github/anrimian/musicplayer/data/storage/source/CompositionSourceEditor.java
@@ -373,7 +373,7 @@ public class CompositionSourceEditor {
     private long editAudioFileTag(String filePath, Long id, ThrowsCallback<Tag> callback)
             throws Exception {
         File fileToEdit = new File(filePath);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) { // || !fileToEdit.canWrite()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R || !fileToEdit.canWrite()) {//will see how it works
             return fileSourceProvider.useTempFile(getFileName(filePath), tempFile -> {
                 copyFileUsingStream(fileToEdit, tempFile);
                 runFileAction(tempFile, callback);
