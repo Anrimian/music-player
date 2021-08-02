8f6e5cd2111e779128efe9ea6cd9b409a011d4c0
Anrimian <anrimian@gmail.com>
2021-08-02 13:05:15 +0300

    + files scanner notes(3)

---
+++ data/src/test/java/com/github/anrimian/musicplayer/data/repositories/scanner/files/FileScannerTest.kt
@@ -0,0 +1,12 @@
+package com.github.anrimian.musicplayer.data.repositories.scanner.files
+
+class FileScannerTest {
+
+
+    //successful scan
+    //error with getting composition from db - after retry do not run next scan
+    //error with scan - after retry set scan time and run next scan
+    //check scanner version set
+    //check scanner version update
+
+}
